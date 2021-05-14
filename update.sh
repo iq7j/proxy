2ccdd4d35ffb878a9113cfadb8a92ef3bd37c8c9
Quack <79198231+iq7j@users.noreply.github.com>
2021-05-14 15:26:18 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,4 +7,5 @@ rm -rif proxy.txt
 rm -rif proxy.py
 wget https://raw.githubusercontent.com/iq7j/proxy/main/proxy.py
 wget https://raw.githubusercontent.com/iq7j/proxy/main/proxy.txt
+chmod +x *
 echo "done"
